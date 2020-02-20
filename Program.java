dfef39e54f20a329d8ae287f3f0eca63d638bde2
Mikhail <formail_2005@mail.ru>
2020-02-20 19:20:22 +0500

    working version + jUnit test

--- Program.java
+++ Program.java
@@ -1,9 +1,6 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
+import java.io.*;
+
+import storage.FactorArray;
 
 public class Program {
 
@@ -16,52 +13,215 @@ public class Program {
 		program.run(args);
 		
 	}
-	void run(String[] args) {
-		compressFileRLE("C:\\1.txt", "C:\\2.txt");
+	void showHelp() {
+		System.out.println("");
+		System.out.println("Использование для компрессии:   rle [-c] <имя исходного(сжимаемого) файла> [<имя файла-результата>]");
+		System.out.println("Использование для декомпрессии: rle -d <имя сжатого файла> <имя файла-результата>");
 	}
-	
-	byte[] compressRLE(byte[] data) {
-		return null;
+	void run(String[] args) {
+		if(args.length == 0) {
+			showHelp();
+			return;
+		}
+		
+		String arg0 = args[0];
+		if(arg0.length() == 0 ) {
+			System.out.println("Неверный параметр \"\".");
+			showHelp();
+			return;
+		}
+		boolean compressMode = true;
+		boolean arg0IsParam = false;
+		if(arg0.charAt(0) == '-') {//если первый символ -, то идентифицируем, что это за параметр
+			arg0 = arg0.toLowerCase();
+			if(arg0.length() != 2 || (arg0.charAt(1) != 'c' && arg0.charAt(1) != 'd') ) {
+				System.out.println("Неверный параметр " + args[0] + ".");
+				showHelp();
+				return;
+			}
+			if(arg0.charAt(1) == 'd')
+				compressMode = false;//т.е. режим декомпрессии
+			arg0IsParam = true;
+		}
+		
+		if(compressMode) {
+			String srcFileName = null;
+			if(arg0IsParam) {
+				if(args.length <= 1) {
+					System.out.println("Необходимо указать имя сжимаемого файла.");
+					return;
+				}
+				else 
+					srcFileName = args[1];
+				
+			}
+			else
+				srcFileName = args[0];
+			if(srcFileName.isEmpty()) {
+				System.out.println("Необходимо указать непустое имя файла.");
+				return;
+			}
+			String compressedFileName = null;
+			if(arg0IsParam) {
+				if(args.length <= 2) 
+					compressedFileName = srcFileName + ".rle";//имя сжатого файла по умолчанию
+				else 
+					compressedFileName = args[2];
+			}
+			else {
+				if(args.length <= 1) 
+					compressedFileName = srcFileName + ".rle";//имя сжатого файла по умолчанию
+				else 
+					compressedFileName = args[1];
+			}
+			if(compressedFileName.isEmpty()) {
+				System.out.println("Необходимо указать непустое имя файла.");
+				return;
+			}
+			compressFileRLE(srcFileName, compressedFileName);
+		}
+		else {//режим декомпрессии, здесь после параметра обязательно указывать 2 имени файла
+			if(args.length < 3) {
+				System.out.println("После параметра \"-d\" необходимо указать файл для декомпресии и имя файла-результата.");
+				return;
+			}
+			String compressedFileName = args[1];
+			String resultFileName = args[2];
+			if(compressedFileName.isEmpty()) {
+				System.out.println("Необходимо указать непустое имя сжатого файла.");
+				return;
+			}
+			if(resultFileName.isEmpty()) {
+				System.out.println("Необходимо указать непустое имя файла-результата.");
+				return;
+			}
+			decompressFileRLE(compressedFileName, resultFileName);
+		}
+		
 	}
 	
 	void compressFileRLE(String inputFileName, String outputFileName) {
 		
 		try {
-			FileInputStream input = new FileInputStream(inputFileName);
-			FileOutputStream output = new FileOutputStream(outputFileName);
+			//FileInputStream input = new FileInputStream(inputFileName);
+			BufferedInputStream input = new BufferedInputStream(new FileInputStream(inputFileName));
+			//FileOutputStream output = new FileOutputStream(outputFileName);
+			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(outputFileName));
 			
-			boolean start = true;
+			int q = 0;
 			byte lastB = 0;
 			int readBufCnt = 0;
 			int cntRLE = 0;
-			byte[] buffer = new byte[256];
+			byte[] readBuffer = new byte[4096*4096];
+			FactorArray<Byte> fa = new FactorArray<Byte>();
 			
 			do {
-				readBufCnt = input.read(buffer);
+				readBufCnt = input.read(readBuffer);
 				for(int i = 0; i < readBufCnt; i++) {
-					byte b = buffer[i];
-					if(start) {
-						start = false;
+					byte b = readBuffer[i];
+					
+					switch(q) {
+					case 0:
 						lastB = b;
 						cntRLE = 1;
-						continue;
-					}
-					if(lastB == b) {
-						cntRLE++;
-					}
-					else {
-						output.write(cntRLE);
-						output.write(lastB);
-						cntRLE = 1;
-						lastB = b;
+						q = 1;
+						break;
+					case 1:
+						if(b == lastB) {
+							cntRLE++;
+							q = 2;
+						}
+						else {
+							fa.add(lastB);
+							lastB = b;
+							//cntRLE = 1;
+							q = 3;
+						}
+						break;
+					case 2:
+						if(b != lastB || cntRLE == 127) {
+							output.write(cntRLE);
+							output.write(lastB);
+							lastB = b;
+							cntRLE = 1;
+							q = 1;
+						}
+						else {
+							cntRLE++;
+						}
+						break;
+					case 3:
+						if(fa.size() == 127) {
+							//TODO здесь можно предусмотреть развилку для ситуаций когда b == lastB и b != lastB
+							//.. для b != lastB, понятно дело, нужно fa.add(lastB); и выводить fa в файл, q = 1
+							//.. b == lastB можно вывести fa в файл(127символов из fa), cnt = 2, q = 2
+							//.. подумать, какой вариант выгоднее с точки зрения эффективности компрессии
+							fa.add(lastB);
+							//write fa:
+							writeFaToFile(fa, output);
+							lastB = b;
+							cntRLE = 1;
+							q = 1;
+							break;
+						}
+						if(b != lastB) {
+							fa.add(lastB);
+							lastB = b;
+							//cntRLE = 1;
+						}
+						else {
+							cntRLE = 2;
+							q = 4;
+						}
+						break;
+					case 4:
+						if(b != lastB) {
+							fa.add(lastB);
+							fa.add(lastB);
+							lastB = b;
+							cntRLE = 1;
+							if(fa.size() == 128) {
+								writeFaToFile(fa, output);
+								q = 1;
+							}
+							else {
+								q = 3;
+							}
+						}
+						else {
+							cntRLE = 3;
+							writeFaToFile(fa, output);
+							q = 2;
+						}
+						break;
+					default:
+						break;
 					}
 					
 				}
 				
 			}while(readBufCnt > 0);
-			if(!start) {
+			
+			switch(q) {
+			case 1:
 				output.write(cntRLE);
 				output.write(lastB);
+				break;
+			case 2:
+				output.write(cntRLE);
+				output.write(lastB);
+				break;
+			case 3:
+				fa.add(lastB);
+				writeFaToFile(fa, output);
+				break;
+			case 4:
+				fa.add(lastB);
+				fa.add(lastB);
+				writeFaToFile(fa, output);
+				break;
+			default:
+				break;
 			}
 			
 			input.close();
@@ -73,13 +233,91 @@ public class Program {
 			return;
 		}
 		
-		
-		
-		
 	}
 	
-	boolean doesFileExist(String fileName) {
-		return false;
+	void decompressFileRLE(String inputFileName, String outputFileName) {
+		try {
+			//FileInputStream input = new FileInputStream(inputFileName);
+			BufferedInputStream input = new BufferedInputStream(new FileInputStream(inputFileName));
+			//FileOutputStream output = new FileOutputStream(outputFileName);
+			BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(outputFileName));
+			
+			int readBufCnt = 0;
+			byte[] readBuffer = new byte[4096];
+			int q = 0;
+			int n = 0;
+			do {
+				readBufCnt = input.read(readBuffer);
+				for(int i = 0; i < readBufCnt && q != 3; i++) {
+					byte b = readBuffer[i];
+					switch(q) {
+					case 0:
+						if(b > 0) {
+							n = b;
+							q = 1;
+							break;
+						}
+						if(b < 0) {
+							n = -b;
+							q = 2;
+						}
+						else {
+							q = 3;
+						}
+						break;
+					case 1:
+						for(int j = 0; j < n; j++) 
+							output.write(b);
+						q = 0;
+						n = 0;
+						break;
+					case 2:
+						output.write(b);
+						n--;
+						if(n == 0)
+							q = 0;
+						break;
+					default:
+						break;
+					}
+					
+				}
+				
+			}while(readBufCnt > 0 && q != 3);
+			
+			switch(q) {
+			case 1://обработка ошибки, когда должен быть еще минимум один символ(для простого RLE)
+				System.out.println("Ошибка формата RLE: ожидание символа, но был достигнут конец файла");
+				break;
+			case 2://обработка ошибки, когда пришёл конец файла, но по-идее еще должны были быть данные
+				System.out.println("Ошибка формата RLE: ожидание " + n + " символа(ов), но был достигнут конец файла");
+				break;
+			case 3://обработка ошибки "n = 0"
+				System.out.println("Ошибка формата RLE: ожидание положительного или отрицательного значения, получено 0x00");
+				break;
+			default:
+				break;
+			}
+			
+			input.close();
+			output.close();
+			if(q == 1 || q == 2 || q == 3) {
+				File file = new File(outputFileName);
+				if(!file.delete())
+					System.out.println("Ошибка удаления файла " + outputFileName);
+			}
+		}
+		catch(Exception ex) {
+			System.out.println(ex.getClass() + ": "+ ex.getMessage());
+			return;
+		}
+	}
+	
+	void writeFaToFile(FactorArray<Byte> fa, OutputStream fOutStream) throws IOException {
+		fOutStream.write(-fa.size());
+		for(int j = 0; j < fa.size() ; j++) 
+			fOutStream.write(fa.get(j));
+		fa.clear();
 	}
 	
 
---
+++ TestCompressDecompress.java
@@ -0,0 +1,144 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import storage.FactorArray;
+
+import java.io.*;
+import java.util.Arrays;
+
+class TestCompressDecompress {
+	
+	FactorArray<byte[]> dataList = new FactorArray<byte[]>();
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		dataList.add(new byte[]{});
+		dataList.add(new byte[]{25, 31, 85, 17, 01, 05, 05, 05, 05, 05, 05, 12, -128, 127, 100, 88, 88, 77, 77, 15});
+		dataList.add(new byte[]{55});
+		dataList.add(new byte[]{75, 62, 12, 15, 75, 95, 100, 111, 111, 111, 111, 111});
+		dataList.add(new byte[]{1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2});
+		
+		byte[] bArr = new byte[1000];
+		Arrays.fill(bArr, (byte)0);
+		for(int i = 0; i < 127; i++)
+			bArr[i] = 55;
+		for(int i = 127; i < (127 + 129); i++)
+			bArr[i] = 55;
+		dataList.add(bArr);
+		
+		bArr = new byte[500];
+		Arrays.fill(bArr, (byte)0);
+		bArr[0] = 55;
+		bArr[1] = 55;
+		bArr[2] = 55;
+		for(int i = 3; i < (3 + 126); i++)
+			bArr[i] = (byte)i;
+		for(int i = (3 + 126); i < (3 + 126 + 2); i++)
+			bArr[i] = 99;
+		for(int i = (3 + 126 + 2); i < (3 + 126 + 2 + 10); i++)
+			bArr[i] = 99;
+		dataList.add(bArr);
+		
+		bArr = new byte[500];
+		Arrays.fill(bArr, (byte)0);
+		bArr[0] = 55;
+		bArr[1] = 55;
+		bArr[2] = 55;
+		for(int i = 3; i < (3 + 126); i++)
+			bArr[i] = (byte)i;
+		for(int i = (3 + 126); i < (3 + 126 + 2); i++)
+			bArr[i] = 77;
+		for(int i = (3 + 126 + 2); i < (3 + 126 + 2 + 10); i++)
+			bArr[i] = 99;
+		dataList.add(bArr);
+		
+		bArr = new byte[500];
+		Arrays.fill(bArr, (byte)0);
+		bArr[0] = 55;
+		for(int i = 1; i < (1 + 128); i++)
+			bArr[i] = (byte)(i/2);
+		dataList.add(bArr);
+		
+		bArr = new byte[500];
+		Arrays.fill(bArr, (byte)0);
+		bArr[0] = 55;
+		for(int i = 1; i < (1 + 127); i++)
+			bArr[i] = (byte)i;
+		dataList.add(bArr);
+		
+		for(int i = 0; i < dataList.size(); i++) {
+			FileOutputStream fos = new FileOutputStream(i + ".bin");
+			fos.write(dataList.get(i));
+			fos.close();
+		}
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		
+		for(int i = 0; i < dataList.size(); i++) {
+			File file = new File(i + ".bin");
+			file.delete();
+			file = new File(i + ".bin.rle");
+			file.delete();
+			file = new File(i + ".bin.dc");
+			file.delete();
+		}
+		dataList = null;
+		
+	}
+
+	@Test
+	void test() {
+		Program pr = new Program();
+		for(int i = 0; i < dataList.size(); i++) {
+			String fileName = i + ".bin";
+			String comprFileName = fileName + ".rle";
+			String decomprFileName = fileName + ".dc";
+			System.out.print("Testing compressing/decompressing '" + fileName + "' ...");
+			pr.compressFileRLE(fileName, comprFileName);
+			pr.decompressFileRLE(comprFileName, decomprFileName);
+			boolean filesEqual = areFilesEqual(fileName, decomprFileName);
+			System.out.println(filesEqual?"OK":"FAIL");
+			assertTrue(filesEqual);
+		}
+	}
+	
+	boolean areFilesEqual(String fileName1, String fileName2) {
+		File file1 = new File(fileName1);
+		File file2 = new File(fileName2);
+		if(file1.length() != file2.length())
+			return false;
+		
+		FileInputStream inf1 = null;
+		FileInputStream inf2 = null;
+		
+		try {
+			inf1 = new FileInputStream(fileName1);
+			inf2 = new FileInputStream(fileName2);
+			int b1 = 0;
+			int b2 = 0;
+			while((b1 = inf1.read()) >=0 ) {
+				b2 = inf2.read();
+				if(b1 != b2) {
+					inf1.close();
+					inf2.close();
+					return false;
+				}
+			}
+			
+			inf1.close();
+			inf2.close();
+		}
+		catch(Exception e) {
+			System.out.println(e.getClass()+ ": "+e.getMessage());
+			return false;
+		}
+		
+		return true;
+	}
+
+}
